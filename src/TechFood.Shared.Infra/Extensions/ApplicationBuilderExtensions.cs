5587721cc614714009791bcfdae44d4f05e64267
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-11-10 15:46:49 -0300

    Add RunMigration extension and update EF Core dependencies
    Added a `RunMigration<DbContext>` extension method to simplify
    runtime database migrations. Updated `TechFood.Shared.Infra.csproj`
    to include `Microsoft.EntityFrameworkCore.Relational` for relational

--- src/TechFood.Shared.Infra/Extensions/ApplicationBuilderExtensions.cs
+++ src/TechFood.Shared.Infra/Extensions/ApplicationBuilderExtensions.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.DependencyInjection;
+using TechFood.Shared.Infra.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Microsoft.AspNetCore.Builder
 {
     public static class ApplicationBuilderExtensions
@@ -6,6 +10,14 @@ namespace Microsoft.AspNetCore.Builder
         {
             app.UseMiddleware<TechFood.Shared.Infra.EventualConsistency.Middleware>();
 
+            return app;
+        }
+
+        public static IApplicationBuilder RunMigration<DbContext>(this IApplicationBuilder app) where DbContext : TechFoodContext
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+            context.Database.Migrate();
             return app;
         }
     }
